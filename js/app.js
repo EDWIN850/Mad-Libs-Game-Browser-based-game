83af2043236ef3c69f624871a78908ab42232223
Edwin Alfaro <ealfaro697@gmail.com>
2022-02-14 10:59:32 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let verb = prompt("Enter a present tense verb");
+let adjective = prompt("Enter a adjective");
+let noun = prompt("Enter a noun");
+let pluralNoun = prompt("Enter a plural noun");
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
