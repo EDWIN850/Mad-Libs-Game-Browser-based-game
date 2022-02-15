019b82f5a9372f2ab44a0d2ca57cf8194357b156
Edwin Alfaro <ealfaro697@gmail.com>
2022-02-15 09:47:41 -0500

    tenth commit

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let verb = prompt("Enter a present tense verb");
 let adjective = prompt("Enter a adjective");
-let noun = prompt("Enter a noun");
+let noun = prompt("Enter a noun; number");
 let pluralNoun = prompt("Enter a plural noun");
-
+let Summer = prompt("Something you do in the summer")
 
 
 
