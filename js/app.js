a3cab7c48e350b1608be411f6f2b5a9e1d4f03f2
Edwin Alfaro <ealfaro697@gmail.com>
2022-02-15 09:29:41 -0500

    ninth commit

--- js/app.js
+++ js/app.js
@@ -12,7 +12,10 @@ let pluralNoun = prompt("Enter a plural noun");
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
 
 
 /*----------------------------- Event Listeners -----------------------------*/
