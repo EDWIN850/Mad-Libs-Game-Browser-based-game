5ca9b9f906c7ed1225a6391e75987a6a7a648672
Edwin Alfaro <ealfaro697@gmail.com>
2022-02-17 12:31:33 -0500

    32 commit

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 /*-------------------------------- Constants --------------------------------*/
 const MadLibsForm = document.getElementById('MadLibs-form');
 const storySection = document.getElementById('completed-story');
-console.log(MadLibsForm)
+
 
 /*---------------------------- Variables (state) ----------------------------*/
+let userSubmission = {}
 const story = `
 <h3>Your completed story:</h3>
-<p>Summer is one of the  <span class="inserted-text">${userSubmission.noun,number} </span> seasons. 
-The other three seasons are <span class="inserted-text">${userSubmission.noun}</span>, and <span class="inserted-text">${userSubmission.adjective}</span>
-<span class="inserted-text"> ${userSubmission.noun}</span>.
-Two major U.S. summer holidays are <span class="inserted-text">${userSubmission.adjective}</span> Day
-and <span class="inserted-text">${userSubmission.adjective}</span> Day.
-Summer is a(n) <span class="inserted-text">${userSubmission.adjective} </span>
-time of the year, when the days are <span class="inserted-text">${userSubmission.adjective}</span> and the
-<span class="inserted-text">${userSubmission.noun}</span>  <span class="inserted-text">${userSubmission.verb}</span>s in the sky all day long. 
- One of the most popular summmer treats is ice <span class="inserted-text">${userSubmission.noun,typeoffood}</span>
- with <span class="inserted-text">${userSubmission.noun,number}</span> gallons sold each year./p>
+<p>Summer is one of the  <span class="inserted-text">${userSubmission.noun1}</span> seasons. 
+The other three seasons are <span class="inserted-text">${userSubmission.noun2}</span>, and <span class="inserted-text">${userSubmission.adjective1}</span>
+<span class="inserted-text"> ${userSubmission.noun3}</span>.
+Two major U.S. summer holidays are <span class="inserted-text">${userSubmission.adjective2}</span> Day
+and <span class="inserted-text">${userSubmission.adjective3}</span> Day.
+Summer is a(n) <span class="inserted-text">${userSubmission.adjective4} </span>
+time of the year, when the days are <span class="inserted-text">${userSubmission.adjective5}</span> and the
+<span class="inserted-text">${userSubmission.noun4}</span>  <span class="inserted-text">${userSubmission.verb}</span>s in the sky all day long. 
+ One of the most popular summmer treats is ice <span class="inserted-text">${userSubmission.noun5}</span>
+ with <span class="inserted-text">${userSubmission.noun6}</span> gallons sold each year./p> `;
+
+ storySection.innerHTML += story;
+ storySection.classList.remove('hide');
+
 
 
 
@@ -40,5 +45,5 @@ MadLibsForm.addEventListener("submit", function (evt) {
 const submitMadLibs = (event) => {
   alert('Hey!');
   event.preventDefault();
-  MadLibsForm
-}) 
+  MadLibsForm.classList.add('hide');
+}
