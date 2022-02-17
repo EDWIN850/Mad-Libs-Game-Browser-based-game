face071d6ae501f6d808c96ee2fc478d85db2dc8
Edwin Alfaro <ealfaro697@gmail.com>
2022-02-17 18:31:57 -0500

    36 commit

--- js/app.js
+++ js/app.js
@@ -54,19 +54,45 @@ const submitMadLibs = (event) => {
   //event.preventDefault();
   MadLibsForm.classList.add('hide');
   userSubmission.noun1 = noun1Input.value
+  
   userSubmission.noun2 = noun2Input.value
+  
   userSubmission.adjective1 = adjective1Input.value
+  
   userSubmission.noun3 = noun3Input.value
+  
   userSubmission.adjective2 = adjective2Input.value 
+  
   userSubmission.adjective3 = adjective3Input.value
+  
   userSubmission.adjective4 = adjective4Input.value
+  
   userSubmission.adjective5 = adjective5Input.value
+  
   userSubmission.noun4 = noun4Input.value
+  
   userSubmission.verb = verbInput.value
+ 
   userSubmission.noun5 = noun5Input.value
+  
   userSubmission.noun6 = noun6Input.value
   
-
+  
+  const story = `
+  <h3>Your completed story:</h3>
+  <p>Summer is one of the  <span class="inserted-text">${userSubmission.noun1}</span> seasons. 
+  The other three seasons are <span class="inserted-text">${userSubmission.noun2}</span>, and <span class="inserted-text">${userSubmission.adjective1}</span>
+  <span class="inserted-text"> ${userSubmission.noun3}</span>.
+  Two major U.S. summer holidays are <span class="inserted-text">${userSubmission.adjective2}</span> Day
+  and <span class="inserted-text">${userSubmission.adjective3}</span> Day.
+  Summer is a(n) <span class="inserted-text">${userSubmission.adjective4} </span>
+  time of the year, when the days are <span class="inserted-text">${userSubmission.adjective5}</span> and the
+  <span class="inserted-text">${userSubmission.noun4}</span>  <span class="inserted-text">${userSubmission.verb}</span>s in the sky all day long. 
+   One of the most popular summmer treats is ice <span class="inserted-text">${userSubmission.noun5}</span>
+   with <span class="inserted-text">${userSubmission.noun6}</span> gallons sold each year. `
+  
+   storySection.innerHTML = story;
+   storySection.classList.remove('hide');
 
 
 } 
