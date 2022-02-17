2f03f52f19f9b9eecd480c61c965b438673fc056
Edwin Alfaro <ealfaro697@gmail.com>
2022-02-17 10:51:28 -0500

    30 commmit

--- js/app.js
+++ js/app.js
@@ -4,6 +4,18 @@ const storySection = document.getElementById('completed-story');
 console.log(MadLibsForm)
 
 /*---------------------------- Variables (state) ----------------------------*/
+const story = `
+<h3>Your completed story:</h3>
+<p>Summer is one of the  <span class="inserted-text">${userSubmission.noun,number} </span> seasons. 
+The other three seasons are <span class="inserted-text">${userSubmission.noun}</span>, and <span class="inserted-text">${userSubmission.adjective}</span>
+<span class="inserted-text"> ${userSubmission.noun}</span>.
+Two major U.S. summer holidays are <span class="inserted-text">${userSubmission.adjective}</span> Day
+and <span class="inserted-text">${userSubmission.adjective}</span> Day.
+Summer is a(n) <span class="inserted-text">${userSubmission.adjective} </span>
+time of the year, when the days are <span class="inserted-text">${userSubmission.adjective}</span> and the
+<span class="inserted-text">${userSubmission.noun}</span>  <span class="inserted-text">${userSubmission.verb}</span>s in the sky all day long. 
+ One of the most popular summmer treats is ice <span class="inserted-text">${userSubmission.noun,typeoffood}</span>
+ with <span class="inserted-text">${userSubmission.noun,number}</span> gallons sold each year./p>
 
 
 
@@ -26,5 +38,7 @@ MadLibsForm.addEventListener("submit", function (evt) {
 
 /*-------------------------------- Functions --------------------------------*/
 const submitMadLibs = (event) => {
-  alert('HELLO!');
+  alert('Hey!');
+  event.preventDefault();
+  MadLibsForm
 }
